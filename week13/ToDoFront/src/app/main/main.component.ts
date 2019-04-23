8e79b4df33cc3a66667d9bef5858c63b51cf5e0c
Hafsa Mufassir <hafsamufassir@gmail.com>
2019-04-23 22:20:37 +0600

    upd

--- week13/ToDoFront/src/app/main/main.component.ts
+++ week13/ToDoFront/src/app/main/main.component.ts
@@ -5,6 +5,7 @@ import {ITaskList, ITask} from '../shared/models/models';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
+  // styleUrls: ['./main.component.css']
 })
 
 export class MainComponent implements OnInit {
